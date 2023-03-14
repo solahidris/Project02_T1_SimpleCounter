f90870a5e4269b452ec65fee30a644be756fbd20
solahidris <emailsolah@gmail.com>
2023-03-14 17:07:31 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*
+  You will build a simple React App which 
+  increment/decrements count on-screen
+  when buttons are clicked
+  using React State.element 
+*/
+
 function App() {
   return (
     <div className="App">
